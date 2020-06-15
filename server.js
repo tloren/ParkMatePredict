58008273b3815643e530289f108062095bf4da8e
Trevor Neil Loren <tloren@deakin.edu.au>
2020-06-15 16:57:19 +1000

    Changed flask app to Node.js

---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var cfenv = require("cfenv");
+var request = require("request");
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const btoa = require("btoa");
+const wml_credentials = new Map();
+
+// -------------------Prediction--------------------
+function apiPost(scoring_url, token, mlInstanceID, payload, loadCallback, errorCallback){
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", loadCallback);
+    oReq.addEventListener("error", errorCallback);
+    oReq.open("POST", scoring_url);
+    oReq.setRequestHeader("Accept", "application/json");
+    oReq.setRequestHeader("Authorization", token);
+    oReq.setRequestHeader("ML-Instance-ID", mlInstanceID);
+    oReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    oReq.send(payload);
+}
+
+
+app.get('/api/test_message', function(req, res) {
+    var apikey = process.env.apikey//"I-vh0iOsv423UQKIu9HTAm7cJu8vO52s2_-oCHVfEmEc";
+
+    // Get an access token from IBM Cloud REST API
+    var options = { url     : "https://iam.bluemix.net/oidc/token",
+                    headers : { "Content-Type"  : "application/x-www-form-urlencoded" },
+                    body    : "apikey=" + apikey + "&grant_type=urn:ibm:params:oauth:grant-type:apikey" };
+    request.post( options, ( error, response, body ) => {
+        var iam_token = JSON.parse( body )["access_token"];
+        const wmlToken = "Bearer " + iam_token;
+        const mlInstanceId = process.env.ml_instance_id//'41a5e5d0-8f9d-4960-a04d-3e5882764e20';
+        values = JSON.stringify([[0.85714286, 1.        , 1.        , 0.76923077, 0.92957746,0.99186992, 0.99193548, 0.91428571, 0.42857143, 0.66779661,0.91262136, 0.875     , 0.95238095, 0.97794118, 0.94736842,1.        , 1.        , 0.97368421, 1.        , 0.89690722,0.97747748, 0.98717949, 0.97647059, 0.97916667, 0.87012987,0.97468354, 0.94444444, 0.98412698, 0.99186992, 0.9375    ,0.98305085, 0.95081967, 0.92307692, 1.        , 0.85714286,0.98666667, 0.97014925, 0.96666667, 0.93333333, 1.        ,0.83333333, 0.94117647, 0.95652174, 0.82325581, 0.87096774,1.        , 0.88888889],[0.85714286, 0.91666667, 0.95238095, 0.76923077, 0.91549296,1.        , 0.99193548, 0.91428571, 0.42857143, 0.66440678,0.88834951, 0.8125    , 0.98412698, 0.94852941, 0.94736842,1.        , 0.90909091, 0.97368421, 1.        , 0.8556701 ,0.98198198, 0.97435897, 0.97647059, 1.        , 0.86363636,0.97468354, 1.        , 1.        , 0.96747967, 0.9375    ,0.94915254, 0.96721311, 1.        , 1.        , 0.85714286,0.98666667, 0.91044776, 0.93333333, 1.        , 1.        ,0.91666667, 0.94117647, 0.91304348, 0.82790698, 0.85483871,0.98214286, 1.        ],[0.85714286, 1.        , 1.        , 0.76923077, 0.91549296,1.        , 0.99193548, 0.91428571, 0.42857143, 0.67118644,0.86893204, 0.9375    , 0.96825397, 0.93382353, 0.89473684,0.91666667, 1.        , 0.94736842, 1.        , 0.88659794,0.92792793, 0.88461538, 0.92941176, 0.9375    , 0.85714286,0.97468354, 1.        , 0.98412698, 0.98373984, 0.875     ,0.84745763, 0.93442623, 0.92307692, 0.93548387, 0.85714286,0.98666667, 0.95522388, 0.95      , 0.93333333, 1.        ,1.        , 0.88235294, 0.95652174, 0.82325581, 0.85483871,0.96428571, 1.        ]]);
+        const payload = '{"fields": [], "values": ['+values+']}';
+        const scoring_url = "https://us-south.ml.cloud.ibm.com/v3/wml_instances/"+mlInstanceId+"/deployments/51217d31-8eb7-4dfc-9eb4-69ba1550a131/online";
+        apiPost(scoring_url, wmlToken, mlInstanceId, payload, function (resp) {
+            let parsedPostResponse;
+            try {
+                parsedPostResponse = JSON.parse(this.responseText);
+            } catch (ex) {
+                // TODO: handle parsing exception
+            }
+            console.log("Scoring response");
+            console.log(parsedPostResponse);
+            res.json(parsedPostResponse.values[0][0])
+        }, function (error) {
+            console.log(error);
+        });
+
+    });
+})
+
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
+
+
